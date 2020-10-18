b9596f2a475775ae9021bf8480ba6733cb872b5b
norman.sanchez <normansanchezn@gmail.com>
2020-10-18 00:15:25 -0500

    [ADD] Creando la primera navegación

--- TragosApp/app/src/main/java/com/empresa/tragos/MainActivity.kt
+++ TragosApp/app/src/main/java/com/empresa/tragos/MainActivity.kt
@@ -2,10 +2,29 @@ package com.empresa.tragos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Este navController nos ayuda a ver las navegaciones
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Con esto el navController sabe que tiene hijos y un stack de los fragments
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
---
+++ TragosApp/app/src/main/java/com/empresa/tragos/ui/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.empresa.tragos.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.empresa.tragos.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnGoDetalles.setOnClickListener {
+            findNavController().navigate(R.id.tragosDetalleFragment)
+        }
+
+    }
+}
