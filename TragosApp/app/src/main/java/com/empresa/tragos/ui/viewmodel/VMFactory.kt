8386dee1fb1fdc13c1d1752b02d32f97167ef10d
norman.sanchez <normansanchezn@gmail.com>
2020-10-18 13:23:37 -0500

    [UPDATE] Comments into VMFactory

--- TragosApp/app/src/main/java/com/empresa/tragos/ui/viewmodel/VMFactory.kt
+++ TragosApp/app/src/main/java/com/empresa/tragos/ui/viewmodel/VMFactory.kt
@@ -6,7 +6,10 @@ import com.empresa.tragos.domain.Repo
 
 /**
  * Created by Norman Sánchez on 18/10/20.
+ * This class help us how to create a constructor without DI, so DI is manual using this.
+ * We need use a helper for create that dependencies using Hild or maybe Dagger II.
  */
+
 class VMFactory(private val repo:Repo):ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(Repo::class.java).newInstance(repo)
